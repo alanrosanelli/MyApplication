8455d6b8cf0a81c653cbc7b5c07030f5bcc86efe
Alan Rosanelli <alan_rosaneli@hotmail.com>
2016-04-12 11:02:35 -0300

    lisview simples work

--- app/src/main/java/com/example/todeolho/myapplication/MainActivity.java
+++ app/src/main/java/com/example/todeolho/myapplication/MainActivity.java
@@ -5,9 +5,12 @@ import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.ListView;
+
+import com.example.todeolho.myapplication.classes.Convenio;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
@@ -19,35 +22,41 @@ import org.apache.http.params.HttpParams;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
-import java.net.URLEncoder;
-
+import java.util.ArrayList;
 
 public class MainActivity extends ActionBarActivity {
-
+    ArrayList<String> convenioList = new ArrayList<String>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         Button getData = (Button) findViewById(R.id.getservicedata);
+
+
         getData.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                String restURL = "http://dadosabertos.rio.rj.gov.br/apiTransporte/apresentacao/rest/index.cfm/onibus";
+                EditText userinput = (EditText) findViewById((R.id.userinput));
+                String restURL = "http://api.convenios.gov.br/siconv/v1/consulta/convenios.json?" +"uf=" + userinput.getText();
                 new RestOperation().execute(restURL);
+
+
             }
         });
+
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,convenioList);
+
+        ListView lv= (ListView) findViewById(R.id.list);
+        lv.setAdapter(adapter);
+
+
     }
 
     public class RestOperation extends AsyncTask<String ,Void, Void> {
@@ -56,100 +65,56 @@ public class MainActivity extends ActionBarActivity {
         String content;
         String error;
         ProgressDialog progressDialog = new ProgressDialog(MainActivity.this);
-        String data;
-        TextView serverDataReceived = (TextView) findViewById(R.id.serverDataReceived);
-        TextView showParsedJSON = (TextView) findViewById(R.id.showParsedJson);
-        EditText userinput = (EditText) findViewById((R.id.userinput));
-
 
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
 
-            progressDialog.setTitle("Please wait...");
+            progressDialog.setTitle("Por Favor espere...");
             progressDialog.show();
 
-            try {
-                data += "&" + URLEncoder.encode("data","UTF-8") + "=" + userinput.getText();
-            } catch (UnsupportedEncodingException e) {
-                e.printStackTrace();
-            }
         }
 
         @Override
         protected Void doInBackground(String... params) {
-            BufferedReader br = null;
 
-            //URL url;
+            InputStream inputStream = null;
+
             try{
-                //url = new URL(params[0]);
-                String url = "http://api.convenios.gov.br/siconv/v1/consulta/municipios.json";
+
+                String url = params[0];
 
                 HttpParams httpParameters=new BasicHttpParams();
-                int timeoutConnection=10000;
+                int timeoutConnection=90000;
                 HttpConnectionParams.setConnectionTimeout(httpParameters, timeoutConnection);
-                int timeoutSocket=10000;
+                int timeoutSocket=90000;
                 HttpConnectionParams.setSoTimeout(httpParameters, timeoutSocket);
 
-                InputStream inputStream=null;
+                HttpClient httpclient = new DefaultHttpClient(httpParameters);
 
-                HttpClient httpclient=new DefaultHttpClient(httpParameters);
-
-                // Cria o GET e define a URL
-                HttpGet httpget=new HttpGet(url);
+                HttpGet httpget = new HttpGet(url);
                 httpget.setHeader("Content-Type", "application/json");
                 httpget.setHeader("Accept", "application/json");
                 HttpResponse httpResponse = httpclient.execute(httpget);
                 inputStream = httpResponse.getEntity().getContent();
 
-                String resultado="";
-                resultado=convertInputStreamToString(br,inputStream);
-                content = resultado;
-
-               /* URLConnection connection = url.openConnection();
-                connection.setDoOutput(true);
-
-                OutputStreamWriter outputStreamWr = new OutputStreamWriter(connection.getOutputStream());
-                outputStreamWr.write(data);
-                outputStreamWr.flush();
-
-                br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-                StringBuilder sb = new StringBuilder();
-                String line = null;
-
-                while((line = br.readLine())!=null){
-
-                    sb.append(line);
-                    sb.append(System.getProperty("line.separator"));
-                }*/
-
-               // content = sb.toString();
+                content = convertInputStreamToString(inputStream);
 
             }catch (MalformedURLException e){
                 e.printStackTrace();
             } catch (IOException e) {
                 e.printStackTrace();
-            } finally {
-                /*try{
-                    br.close();
-
-                }catch (IOException e){
-                    e.printStackTrace();
-                }*/
             }
             return null;
         }
 
-        private  String convertInputStreamToString(BufferedReader bufferedReader,InputStream inputStream) throws IOException
+        private  String convertInputStreamToString(InputStream inputStream) throws IOException
         {
-             bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "ISO-8859-1"));
+            BufferedReader bufferedReader  = new BufferedReader(new InputStreamReader(inputStream, "ISO-8859-1"));
 
             int c;
             StringBuilder response=new StringBuilder();
 
-            // Lê do buffer pegando caracter a caracter. Como particularmente este
-            // fluxo consumido não possui fim de linha
-            // com newline (\n), o método readLine() não funciona.
             while ((c=bufferedReader.read()) != -1)
             {
                 response.append((char) c);
@@ -159,7 +124,6 @@ public class MainActivity extends ActionBarActivity {
             bufferedReader.close();
 
             return result;
-
         }
 
         @Override
@@ -169,9 +133,9 @@ public class MainActivity extends ActionBarActivity {
             progressDialog.dismiss();
 
             if(error!=null){
-                serverDataReceived.setText("Error " + error);
+
             } else {
-                serverDataReceived.setText(content);
+
 
                 String output = "";
                 JSONObject jsonResponse;
@@ -179,17 +143,22 @@ public class MainActivity extends ActionBarActivity {
                 try {
                     jsonResponse = new JSONObject(content);
 
-                    JSONArray jsonArray = jsonResponse.optJSONArray("municipios");
+                    JSONArray jsonArray = jsonResponse.optJSONArray("convenios");
 
                     for (int i = 0; i < jsonArray.length(); i++) {
                         JSONObject child = jsonArray.getJSONObject(i);
 
-                        String name = child.getString("nome");
+                        Convenio convenio =  new Convenio();
+                        convenio.setModalidade(child.getString("modalidade"));
+                        convenioList.add(convenio.getModalidade());
 
-                        output = "Nome=" + name;
+
                     }
 
-                    showParsedJSON.setText(output);
+
+                    ListView lv= (ListView) findViewById(R.id.list);
+                    lv.invalidateViews();
+                    //showParsedJSON.setText(output);
 
                 } catch (JSONException e) {
                     e.printStackTrace();
