63526ee603924ae1af2ccf82cb00bed512db4ecf
Alan Rosanelli <alan_rosaneli@hotmail.com>
2016-05-31 05:08:42 -0300

    facebook autentication
    autenticacao funcionando

--- app/src/main/java/com/example/todeolho/myapplication/MainActivity.java
+++ app/src/main/java/com/example/todeolho/myapplication/MainActivity.java
@@ -3,37 +3,52 @@ package com.example.todeolho.myapplication;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
-
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 
-public class MainActivity extends ActionBarActivity implements OnItemClickListener, OnItemSelectedListener{
+public class MainActivity extends AppCompatActivity implements OnItemClickListener, OnItemSelectedListener{
 
     AutoCompleteTextView textView=null;
     private ArrayAdapter<String> adapter;
+    //LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
+    CallbackManager callbackManager;
+
 
     String item[]={
             "Taquara - RS - 13466","Parobe - RS - 13579","Igrejinha - RS - 15963"
     };
 
-
-
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+        facebookSDKInitialize();
         setContentView(R.layout.activity_main);
+        LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions("email");
+        getLoginDetails(loginButton);
+
 
         textView = (AutoCompleteTextView) findViewById(R.id.txtMunicipio);
         adapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, item);
@@ -63,11 +78,68 @@ public class MainActivity extends ActionBarActivity implements OnItemClickListen
                 startActivity(secondActivity.putExtras(bundle));
             }
         });
-
-
-
-
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        Log.e("data",data.toString());
+    }
+
+
+    protected void getLoginDetails(LoginButton login_button){
+
+        // Callback registration
+        login_button.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult login_result) {
+                Intent intent = new Intent(MainActivity.this,HomeActivity.class);
+                startActivity(intent);
+
+            }
+
+            @Override
+            public void onCancel() {
+                // code for cancellation
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                //  code to handle error
+            }
+        });
+    }
+
+
+    protected void facebookSDKInitialize() {
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        callbackManager = CallbackManager.Factory.create();
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Logs 'install' and 'app activate' App Events.
+        AppEventsLogger.activateApp(this);
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // Logs 'app deactivate' App Event.
+        AppEventsLogger.deactivateApp(this);
+    }
+
+
+
+
+
 
     @Override
     public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
